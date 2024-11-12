3f73ef95c9ac69e6d5ded8e06fc6d6c4ca48b9dc
Talha Juikar <talha.j@netlabindia.com>
2024-11-12 10:24:06 +0000

    update email script

--- scripts/email_conf.sh
+++ scripts/email_conf.sh
@@ -1,51 +1,71 @@
 #!/bin/bash
 
-# Variables
-# SENDER_NAME="<sender_name>"  # Replace with actual sender name
-# USERNAME="<username>"         # Replace with actual username
-# PASSWORD="<password>"         # Replace with actual password
-# SECURE_SETTINGS_PASSWORD="1234"  # Replace with the actual secure settings password
+set -u
+set -o pipefail
 
-# Prompt for user input
+CONTAINER_NAME="os01"
+
 prompt_for_input() {
     read -p "$1: " input
     echo "$input"
 }
 
+prompt_for_password() {
+    systemd-ask-password --echo=no --emoji=no "$1:"
+}
+
 # Check for command-line arguments and assign them to variables
-if [ $# -ge 4 ]; then
+if [ $# -ge 3 ]; then
     SENDER_NAME="$1"
     USERNAME="$2"
     PASSWORD="$3"
-    SECURE_SETTINGS_PASSWORD="$4"
+    KESTORE_PASSWORD="$4"
+    ADMIN_PASSWORD="$5"
 else
     # Prompting for variables if not provided via command line
     SENDER_NAME=$(prompt_for_input "Enter sender name")
     USERNAME=$(prompt_for_input "Enter email username")
-    PASSWORD=$(prompt_for_input "Enter email password")
-    SECURE_SETTINGS_PASSWORD=$(prompt_for_input "Enter secure settings password")
+    PASSWORD=$(prompt_for_password "Enter email password")
+    KESTORE_PASSWORD=$(prompt_for_password "Enter the keystore password")
+    ADMIN_PASSWORD=$(prompt_for_password "Enter admin password")
 fi
 
 
-# Function to add credentials to the keystore
 add_to_keystore() {
-    echo "Adding email username to keystore..."
-    /usr/share/opensearch/bin/opensearch-keystore add opensearch.notifications.core.email.$SENDER_NAME.username <<< "$USERNAME"
+    echo "Adding email username to keystore inside the container..."
+    docker compose exec -it "$CONTAINER_NAME" bash -c "echo -e \"$KESTORE_PASSWORD\\n$USERNAME\" | /usr/share/opensearch/bin/opensearch-keystore add opensearch.notifications.core.email.$SENDER_NAME.username"
     
-    echo "Adding email password to keystore..."
-    /usr/share/opensearch/bin/opensearch-keystore add opensearch.notifications.core.email.$SENDER_NAME.password <<< "$PASSWORD"
+    echo "Adding email password to keystore inside the container..."
+    docker compose exec -it "$CONTAINER_NAME" bash -c "echo -e \"$KESTORE_PASSWORD\\n$PASSWORD\" | /usr/share/opensearch/bin/opensearch-keystore add opensearch.notifications.core.email.$SENDER_NAME.password"
 }
 
-# Function to reload secure settings
+
+set_keystore_password() {
+    echo "Checking if the keystore is already password protected..."
+    docker compose exec -it "$CONTAINER_NAME" bash -c "echo -e \"$KESTORE_PASSWORD\\n\" | /usr/share/opensearch/bin/opensearch-keystore has-passwd" > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo "Keystore is already password protected. No need to set the password."
+    else
+        echo "Keystore is not password protected. Setting keystore password..."
+        docker compose exec -it "$CONTAINER_NAME" bash -c "echo -e \"$KESTORE_PASSWORD\\n$KESTORE_PASSWORD\" | /usr/share/opensearch/bin/opensearch-keystore passwd"
+    fi
+}
+
+
 reload_secure_settings() {
-    echo "Reloading secure settings..."
-    curl -X POST "http://localhost:9200/_nodes/reload_secure_settings" -H "Content-Type: application/json" -d '{
-        "secure_settings_password": "'"$SECURE_SETTINGS_PASSWORD"'"
-    }'
+    echo "Reloading secure settings inside the container..."
+    docker compose exec -it "$CONTAINER_NAME" curl -k -X POST "https://localhost:9200/_nodes/reload_secure_settings" \
+        -H "Content-Type: application/json" \
+        -d '{
+            "secure_settings_password": "'"$KESTORE_PASSWORD"'"
+        }' \
+        -u "admin:$ADMIN_PASSWORD"
 }
 
-# Main execution
+
+set_keystore_password
 add_to_keystore
 reload_secure_settings
 
-echo "Credentials added and secure settings reloaded."
+echo "Keystore password set, credentials added, and secure settings reloaded inside the container."
